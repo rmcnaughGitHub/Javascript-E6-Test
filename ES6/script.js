35b0ce5e5001c0d2121426136627d3746bf8f2a3
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2016-11-14 22:21:16 -0500

    added main files

---
+++ ES6/script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+/*
+ * Either assign object to already 
+ established object or start over!!!
+ */
+var ecmaSix = window.ecmaSix || {};
+
+var ecmaSix = {
+	init: function(){
+
+	},
+
+	run: function(){
+
+	}
+};
+
+//Load Content
+document.addEventListener('DOMContentLoaded', function(event) {
+	ecmaSix.init();
+});
